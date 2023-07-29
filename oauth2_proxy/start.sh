ee8c7e832e2031b08e1960ee39995334de554ddf
ormsng <ormessing96@gmail.com>
2023-07-29 11:34:14 +0300

    Init repo

--- oauth2_proxy/start.sh
+++ oauth2_proxy/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 oauth2_proxy \
- --authenticated-emails-file="$VALID_EMAIL_FILE" \
+ --email-domain=* \
  --upstream=$UPSTREAM \
  --http-address=$HTTP_ADDRESS \
- --approval-prompt=$APPROVAL_PROMPT \
  --redirect-url=$REDIRECT_URL \
  --cookie-secret=$COOKIE_SECRET \
  --cookie-name=$COOKIE_NAME \
